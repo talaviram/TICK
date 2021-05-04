e1fdfbbda2251f77dbe277a2df9ca5c32aaf0102
Tal Aviram <me@talaviram.com>
2021-05-04 08:03:13 +0300

    Build projucer util on Windows (VS2019).

---
+++ build_projucer.bat
@@ -0,0 +1,2 @@
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" ./JUCE/extras/Projucer/Builds/VisualStudio2019/Projucer.sln
+"./JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe" "%~dp0TICK.jucer"
