44a8f8c4edff8133a94add3d4574b248ebdf1312
Tal Aviram <me@talaviram.com>
2021-06-18 09:44:04 +0300

    Add AAX codesign on mac script.

--- build_mac.sh
+++ build_mac.sh
@@ -18,12 +18,15 @@ echo Copying built binaries...
 mkdir -p result/mac
 # Projucer locations
 # TODO: add non-GPL which should be built with different flags
-# cp -R -L ./Builds/MacOSX/build/Release/TICK.aax ./result/mac
+cp -R -L ./Builds/MacOSX/build/Release/TICK.aaxplugin ./result/mac
 cp -R -L ./Builds/MacOSX/build/Release/TICK.app ./result/mac
 cp -R -L ./Builds/MacOSX/build/Release/TICK.component ./result/mac
 cp -R -L ./Builds/MacOSX/build/Release/TICK.vst ./result/mac
 cp -R -L ./Builds/MacOSX/build/Release/TICK.vst3 ./result/mac
-cp -R -L ./Builds/MacOSX/build/Release/TICK.aaxplugin ./result/mac
+
+echo Sign AAX...
+python3 ./private/sign_aax.py ./result/mac/TICK.aaxplugin
+
 
 echo Create Installer...
 packagesbuild ./Installer/TICK.pkgproj
