703cbbbccffacbe43bb5539da95c7291face74e6
Tal Aviram <me@talaviram.com>
2021-06-30 15:56:25 +0300

    Fixed some commenting out for Windows build script.

--- build_win.bat
+++ build_win.bat
@@ -1,5 +1,5 @@
-REM "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" ./JUCE/extras/Projucer/Builds/VisualStudio2019/Projucer.sln
-REM "./JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe" --resave ./TICK.jucer
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" ./JUCE/extras/Projucer/Builds/VisualStudio2019/Projucer.sln
+"./JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe" --resave ./TICK.jucer
 
 echo Building GPL formats
 set CL=/DJUCE_DISPLAY_SPLASH_SCREEN#0
