talaviram/TICK

commit 6e246954d2a618a2b6aa00bd15b94c10c8217ecf

Bumped some versioning.

Source/PluginProcessor.cpp
@@ -196,7 +196,7 @@ void TickAudioProcessor::handlePreCount (const double inputPPQ)
     const auto ts = playheadPosition_.getTimeSignature().orFallback (AudioPlayHead::TimeSignature ({ 4 / 4 }));
     const auto ttq = (4.0 / ts.denominator); // tick to quarter
     const auto expectedBar = std::floor (inputPPQ / ttq / ts.numerator);
-    if (expectedBar == preCount)
+    if ((int)expectedBar == preCount)
         getState().transport.isPlaying.setValue (false, nullptr);
 }
 
@@ -275,7 +275,7 @@ void TickAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer&)
         const auto ts = playheadPosition_.getTimeSignature().orFallback(AudioPlayHead::TimeSignature ({4, 4}));
         
         // calculate where tick starts in samples...
-        jassert (lastBarStart == 0 || ppqPosition >= lastBarStart);
+        jassert ((int)lastBarStart == 0 || ppqPosition >= lastBarStart);
         const auto pos = ppqPosition - lastBarStart;
         const auto bps = bpm / 60.0;
         const auto bpSmp = getSampleRate() / bps;
@@ -300,7 +300,7 @@ void TickAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer&)
         // reset tick state
         tickState.tickStartPosition = 0;
 
-        if (ppqFromBufStart == 0)
+        if (juce::roundToInt (ppqFromBufStart) == 0)
         {
             ppqPosInBuf = 0.0;
         }

Installer/TICK.pkgproj
@@ -2736,7 +2736,7 @@
 															<array>
 																<dict>
 																	<key>BUNDLE_CAN_DOWNGRADE</key>
-																	<false/>
+																	<true/>
 																	<key>BUNDLE_POSTINSTALL_PATH</key>
 																	<dict>
 																		<key>PATH_TYPE</key>

Source/model/TapModel.cpp
@@ -46,7 +46,7 @@ int TapModel::getXforCurrentRange (const juce::Time& t, int areaWidth) const
     const float range = (tapPoints.front() - start).inMilliseconds();
     const auto pos = t - start;
 
-    const auto x = juce::roundToInt (range == 0 ? 1 : (pos.inMilliseconds() / range) * (areaWidth - 1));
+    const auto x = juce::roundToInt (juce::roundToInt (range) == 0 ? 1 : (pos.inMilliseconds() / range) * (areaWidth - 1));
     jassert (x >= 0 && x < areaWidth);
     return x;
 }

Source/utils/TickSplash.cpp
@@ -28,7 +28,7 @@ void TickSplash::paint (Graphics& g)
     area.removeFromBottom (80);
     g.drawFittedText (JucePlugin_Manufacturer ", Copyright 2019-2023", area.removeFromBottom (40), Justification::centredBottom, 1);
     auto logoArea = area.toFloat().reduced (getWidth() * 0.2f);
-    if (logoArea.getHeight() == 0)
+    if (logoArea.toNearestInt().getHeight() == 0)
         logoArea = area.toFloat();
     logo->drawWithin (g, logoArea, juce::RectanglePlacement(), 1.0f);
 

TICK.jucer
@@ -16,7 +16,7 @@
               projectLineFeed="&#10;" defines="JUCE_USE_CUSTOM_PLUGIN_STANDALONE_APP&#10;JUCE_DONT_AUTO_OPEN_MIDI_DEVICES_ON_MOBILE=1&#10;JUCE_COREGRAPHICS_RENDER_WITH_MULTIPLE_PAINT_CALLS"
               pluginCharacteristicsValue="pluginIsSynth,pluginWantsMidiIn"
               jucerFormatVersion="1" addUsingNamespaceToJuceHeader="0" headerPath="../../;&#10;../../Source"
-              pluginAUMainType="'aumu'" cppLanguageStandard="17" version="0.6.1"
+              pluginAUMainType="'aumu'" cppLanguageStandard="17" version="0.6.2"
               useAppConfig="0">
   <MAINGROUP id="C2pL3d" name="TICK">
     <FILE id="Qy9ZQb" name="factory_default.preset" compile="0" resource="1"
@@ -209,7 +209,7 @@
     <ANDROIDSTUDIO targetFolder="Builds/Android" androidPushNotifications="0" androidExtraAssetsFolder="Installer/Factory"
                    androidTheme="@android:style/Theme.NoTitleBar" smallIcon="OKPwIG"
                    bigIcon="OKPwIG" androidEnableContentSharing="1" androidGradleSettingsContent="def task = &quot;cmake -Dlocal_dir=../.. -Doutput_dir=../../Source/utils -P ../../cmake/gitversion.cmake&quot;.execute()&#10;task.waitFor()&#10;println task.text&#10;"
-                   androidVersionCode="5">
+                   androidVersionCode="6" androidReadMediaImagesPermission="0" androidReadMediaVideoPermission="0">
       <CONFIGURATIONS>
         <CONFIGURATION isDebug="1" name="Debug" targetName="TICK"/>
         <CONFIGURATION isDebug="0" name="Release" targetName="TICK"/>

commit c9f6fcb75278a9f0398526dd422011b42ece270a

Fixed regression in 6e246954d2a618a2b6aa00bd15b94c10c8217ecf due to unintended extra committed content.

Source/PluginProcessor.cpp
@@ -300,7 +300,7 @@ void TickAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer&)
         // reset tick state
         tickState.tickStartPosition = 0;
 
-        if (juce::roundToInt (ppqFromBufStart) == 0)
+        if (ppqFromBufStart == 0.0)
         {
             ppqPosInBuf = 0.0;
         }